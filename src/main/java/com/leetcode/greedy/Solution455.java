a4b8a1a87803d15a7fde7b24ff1f56bcaf06d0de
coolxll <coolxll@gmail.com>
2020-03-03 11:50:55 +0800

    Daily Practice

---
+++ src/main/java/com/leetcode/greedy/Solution455.java
@@ -0,0 +1,20 @@
+package com.leetcode.greedy;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution455 {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int cookie =0;
+        int child = 0;
+        while(child < g.length && cookie < s.length) {
+            if(g[child] <= s[cookie]) {
+                child++;
+            }
+            cookie++;
+        }
+        return child;
+    }
+}
---
+++ src/test/java/com/leetcode/greedy/Solution455Test.java
@@ -0,0 +1,17 @@
+package com.leetcode.greedy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution455Test {
+
+    @Test
+    public void findContentChildren() {
+        Solution455 solution455 = new Solution455();
+        int[] g = new int[] {1,2,3};
+        int[] s = new int[]{1, 1};
+        int contentChildren = solution455.findContentChildren(g, s);
+        assert contentChildren == 1;
+    }
+}
