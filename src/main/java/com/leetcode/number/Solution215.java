259ae93b7107c607e07d61143c45d87f242991d9
xulinglong <xulinglong@zhongan.com>
2020-03-17 19:35:35 +0800

    20200306 practise

---
+++ src/main/java/com/leetcode/number/Solution215.java
@@ -0,0 +1,21 @@
+package com.leetcode.number;
+
+import java.util.PriorityQueue;
+
+public class Solution215 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        for (int num : nums) {
+            if (pq.size() < k) {
+                pq.offer(num);
+            } else {
+                int top = pq.peek();
+                if (num > top) {
+                    pq.poll();
+                    pq.offer(num);
+                }
+            }
+        }
+        return pq.peek();
+    }
+}
